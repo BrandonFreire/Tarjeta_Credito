47d3318ccac1e6a824b98f340b3136b4b86dca61
sitas <sebasb1955@hotmail.com>
2023-12-17 15:59:25 -0500

    juridica y natural

--- PersonaJur.java
+++ PersonaJur.java
@@ -1,9 +1,38 @@
-public class PersonaJur extends Cliente{
+public class PersonaJur extends Cliente {
     private String ruc;
-    
-    //METODOS
-    public PersonaJur(String nombre, String ruc, String direccion, String numTelefonico, String email){
-        super(nombre,direccion,numTelefonico,email);
-        this.ruc = ruc;
+
+    // Constructor
+    public PersonaJur(String nombre, String ruc, String direccion, String numTelefonico, String email) {
+        super(nombre, direccion, numTelefonico, email);
+        setRuc(ruc); 
+    }
+
+    // Getter y setter de RUC
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        if (ruc == null || ruc.trim().isEmpty()) {
+            System.out.println("No definido");
+        } else if (ruc.matches("\\d{10}001")) {
+            this.ruc = ruc;
+        } else {
+            System.out.println("RUC no válido.");
+        }
+    }
+
+    public void mostrarPersonaJur() {
+        System.out.println("RUC: " + getRuc());
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Dirección: " + getDireccion());
+        System.out.println("Número de Teléfono: " + getNumTelefonico());
+        System.out.println("Correo Electrónico: " + getEmail());
+    }
+
+    // Método main de ejemplo
+    public static void main(String[] args) {
+        PersonaJur personaJuridica = new PersonaJur("Empresa", "1234567895001", "Avenida Principal", "0123456789", "empresa@ejemplo.com");
+        personaJuridica.mostrarPersonaJur();
     }
 }
--- PersonaNatur.java
+++ PersonaNatur.java
@@ -1,13 +1,54 @@
-public class PersonaNatur extends Cliente{
+public class PersonaNatur extends Cliente {
     private String cedula;
     private String apellido;
 
-    //METODOS
-    public PersonaNatur(String nombre, String apellido, String cedula, String direccion, String numTelefonico, String email){
+    public PersonaNatur(String nombre, String apellido, String cedula, String direccion, String numTelefonico, String email) {
         super(nombre, direccion, numTelefonico, email);
         this.apellido = apellido;
-        this.cedula = cedula;
+        setCedula(cedula); 
     }
-    // getter/setter
 
+    // Getter y setter de cedula
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        if (cedula == null || cedula.trim().isEmpty()) {
+            System.out.println("No definido");
+        } else if (cedula.matches("\\d{10}")) {
+            this.cedula = cedula;
+        } else {
+            System.out.println("Cédula no válida.");
+        }
+    }
+
+    // Getter y setter de apellido
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        if (apellido == null || apellido.trim().isEmpty()) {
+            System.out.println("No definido");
+        } else if (apellido.matches("^[a-zA-ZáéíóúÁÉÍÓÚüÜ\\s]+$")) {
+            this.apellido = apellido;
+        } else {
+            System.out.println("Apellido no válido.");
+        }
+    }
+
+    public void mostrarPersonaNatur() {
+        System.out.println("Cédula: " + getCedula());
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Apellido: " + getApellido());
+        System.out.println("Dirección: " + getDireccion());
+        System.out.println("Número de Teléfono: " + getNumTelefonico());
+        System.out.println("Correo Electrónico: " + getEmail());
+    }
+
+    public static void main(String[] args) {
+        PersonaNatur persona = new PersonaNatur("Juan", "Pérez", "1234567890", "Calle 123", "0123456789", "juan@example.com");
+        persona.mostrarPersonaNatur();
+    }
 }
