512b9f09742493bb16d2e4b8de36501525da28c2
carol230 <caro.ayala2605@gmail.com>
2023-12-17 20:45:45 -0500

    V2 Ivonne

--- Producto.java
+++ Producto.java
@@ -1,53 +1,85 @@
 public class Producto {
-        private String codigo;
-        private String nombre;
-        private String precioVenta;
-        private String precioBeneficio;
-        private String stock;
+    private String codigo;
+    private String nombre;
+    private String precioVenta;
+    private String precioBeneficio;
+    private String stock;
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo.isBlank() ? "Error: El codigo no puede estar en blanco." : codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre.isBlank() ? "Error: El nombre no puede estar en blanco." : nombre;
+    }
+
+    public String getPrecioVenta() {
+    return precioVenta;
+    }
     
-        public void setCodigo(String codigo) {
-            this.codigo = codigo.isBlank() ? "Error: El codigo no puede estar en blanco." : codigo;
-        }
-    
-        public String getNombre() {
-            return nombre;
-        }
-    
-        public void setNombre(String nombre) {
-            this.nombre = nombre.isBlank() ? "Error: El nombre no puede estar en blanco." : nombre;
-        }
-    
-        public String getPrecioVenta() {
-        return precioVenta;
-        }
+
+    public void setPrecioVenta(String precioVenta) {
+        if (precioVenta.isBlank()) {
+            System.out.println("Error: El precio de venta no puede estar en blanco.");
+        } else if (precioVenta.matches("[0-9]+(\\.[0-9]{1,2})?")) {
         
-    
-        public void setPrecioVenta(String precioVenta) {
+            double precio = Double.parseDouble(precioVenta);
+
+        if (precio > 0) {
             this.precioVenta = precioVenta;
+        } else {
+            System.out.println("Error: El precio de venta debe ser mayor que 0.");
         }
-    
-        public String getPrecioBeneficio() {
-            return precioBeneficio;
+        } else {
+            System.out.println("Error: El formato del precio de venta no es válido.");
         }
-    
-        public void setPrecioBeneficio(String precioBeneficio) {
+    }
+
+    public String getPrecioBeneficio() {
+        return precioBeneficio;
+    }
+
+    public void setPrecioBeneficio(String precioBeneficio) {
+        if (precioBeneficio.isBlank()) {
+            System.out.println("Error: El precio de beneficio no puede estar en blanco.");
+        } else if (precioBeneficio.matches("[0-9]+(\\.[0-9]{1,2})?")) {
+            double precio = Double.parseDouble(precioBeneficio);
+
+        if (precio > 0) {
             this.precioBeneficio = precioBeneficio;
+        } else {
+            System.out.println("Error: El precio de venta debe ser mayor que 0.");
         }
-    
-        public String getStock() {
-            return stock;
+        } else {
+            System.out.println("Error: El formato del precio de beneficio no es válido.");
         }
-    
-        public void setStock(String stock) {
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        if (stock.isBlank()) {
+            System.out.println("Error: El stock no puede estar en blanco.");
+        } else if (stock.matches("[0-9]+(\\.[0-9]{1,2})?")) {
             this.stock = stock;
+        } else {
+            System.out.println("Error: El formato del stock no es válido.");
         }
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
     
-        public String getCodigo() {
-            return codigo;
-        }
+
     
-        
-    
-        
-        public Producto(){}
+    //  Constructores
+    public Producto(){}
 }
