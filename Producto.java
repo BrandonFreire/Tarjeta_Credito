224e27f196e0f37e59a99340a5126d7e1b35c524
carol230 <caro.ayala2605@gmail.com>
2023-12-17 21:07:54 -0500

    V3

--- Producto.java
+++ Producto.java
@@ -24,7 +24,8 @@ public class Producto {
 
     public void setPrecioVenta(String precioVenta) {
         if (precioVenta.isBlank()) {
-            System.out.println("Error: El precio de venta no puede estar en blanco.");
+            //System.out.println("Error: El precio de venta no puede estar en blanco.");
+             this.precioVenta = "Error:  El precio de venta no puede estar en blanco.";
         } else if (precioVenta.matches("[0-9]+(\\.[0-9]{1,2})?")) {
         
             double precio = Double.parseDouble(precioVenta);
@@ -32,10 +33,12 @@ public class Producto {
         if (precio > 0) {
             this.precioVenta = precioVenta;
         } else {
-            System.out.println("Error: El precio de venta debe ser mayor que 0.");
+            //System.out.println("Error: El precio de venta debe ser mayor que 0.");
+            this.precioVenta = "Error: El precio de venta debe ser mayor que 0";
         }
         } else {
-            System.out.println("Error: El formato del precio de venta no es válido.");
+            //System.out.println("Error: El formato del precio de venta no es válido.");
+            this.precioVenta = "Error: Formato no válido";
         }
     }
 
@@ -45,31 +48,39 @@ public class Producto {
 
     public void setPrecioBeneficio(String precioBeneficio) {
         if (precioBeneficio.isBlank()) {
-            System.out.println("Error: El precio de beneficio no puede estar en blanco.");
+            //System.out.println("Error: El precio de beneficio no puede estar en blanco.");
+             this.precioBeneficio = "Error:  El precio de beneficio no puede estar en blanco.";
         } else if (precioBeneficio.matches("[0-9]+(\\.[0-9]{1,2})?")) {
+        
             double precio = Double.parseDouble(precioBeneficio);
 
         if (precio > 0) {
             this.precioBeneficio = precioBeneficio;
         } else {
-            System.out.println("Error: El precio de venta debe ser mayor que 0.");
+            //System.out.println("Error: El precio de beneficio debe ser mayor que 0.");
+            this.precioBeneficio = "Error: El precio de beneficio debe ser mayor que 0";
         }
         } else {
-            System.out.println("Error: El formato del precio de beneficio no es válido.");
+            //System.out.println("Error: El formato del precio de beneficio no es válido.");
+            this.precioBeneficio = "Error: Formato no válido";
         }
     }
+    
 
     public String getStock() {
         return stock;
     }
 
     public void setStock(String stock) {
+        
         if (stock.isBlank()) {
-            System.out.println("Error: El stock no puede estar en blanco.");
-        } else if (stock.matches("[0-9]+(\\.[0-9]{1,2})?")) {
+            //System.out.println("Error: El stock no puede estar en blanco.");
+             this.stock = "Error:  El stock no puede estar en blanco.";
+        } else if (stock.matches("[0-9]")) {
             this.stock = stock;
         } else {
-            System.out.println("Error: El formato del stock no es válido.");
+            //System.out.println("Error: El formato del stock no es válido.");
+            this.stock = "Error: Formato no válido";
         }
     }
 
@@ -77,9 +88,28 @@ public class Producto {
         return codigo;
     }
 
+    public void mostrarProducto() {
+        System.out.println("Codigo: " + getCodigo());
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Precio Venta: " + getPrecioVenta());
+        System.out.println("Precio Beneficio: " + getPrecioBeneficio());
+        System.out.println("Stock: " + getStock());
+    }
     
 
     
     //  Constructores
     public Producto(){}
+    public Producto(String codigo, String nombre, String precioVenta, String precioBeneficio, String stock) {
+        setCodigo(codigo);
+        setNombre(nombre);
+        setPrecioVenta(precioVenta);
+        setPrecioBeneficio(precioBeneficio);
+        setStock(stock);
+    }
+    public static void main(String[] args) {
+
+        Producto producto = new Producto("12345", "Lavadora", "136", "126.5", "100");
+        producto.mostrarProducto();
+    }
 }
