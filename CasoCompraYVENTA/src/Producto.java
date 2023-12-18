6574e710265a74bc6465cf1caed10a94bf06f8bf
carol230 <caro.ayala2605@gmail.com>
2023-12-18 18:17:50 -0500

    Cambio a proyecto java

--- CasoCompraYVENTA/src/Producto.java
+++ CasoCompraYVENTA/src/Producto.java
@@ -80,7 +80,7 @@ public class Producto {
         if (stock.isBlank()) {
             //System.out.println("Error: El stock no puede estar en blanco.");
              this.stock = "Error:  El stock no puede estar en blanco.";
-        } else if (stock.matches("[0-9]")) {
+        } else if (stock.matches("[0-9]+")) {
             this.stock = stock;
         } else {
             //System.out.println("Error: El formato del stock no es válido.");
